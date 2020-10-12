600178f91196641ffc245bca2b58e17e27f34c48
Maran <maran@MacBook-Pro-van-Willemien.local>
2020-10-12 11:01:20 +0200

    fixed >$ export YYY=echo >$  hallo

--- execute/check_builtin_again.c
+++ execute/check_builtin_again.c
@@ -6,7 +6,7 @@
 /*   By: maran <maran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/07 17:31:13 by maran         #+#    #+#                 */
-/*   Updated: 2020/10/08 15:36:27 by maran         ########   odam.nl         */
+/*   Updated: 2020/10/12 10:58:21 by maran         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ ls > x1 ; cat x1 | $POEP
 TO DO: 
 $POEP			--> bash: $POEP: command not found 			-->  foutmelding weghalen
 hallo $POEP 	--> bash: hallo: command not found  		--> dan er al uit klappen! Behalve als eerste woord begint met$	
-
+------
+TO DO:
+	- Wat als y geen 0, gaat het dan goed. Welke gevallen is dit?		MEER TESTEN (of vanzelf achterkomen.)
 ------
 ** When there is a $ sign. And there is no known command yet the _env variable should be checked on comments inside the variable.
 ** >$ export LS="ls -la" 
@@ -34,6 +36,14 @@ hallo $POEP 	--> bash: hallo: command not found  		--> dan er al uit klappen! Be
 
 void				check_builtin_again(t_command **command, t_env *_env, int y)
 {
-	if ((*command)->builtin == builtin_no_com)
-		lexer_parser_executer((*command)->array[y], 0, &_env);
+	if ((*command)->builtin == builtin_no_com && y == 0)
+	{
+		while ((*command)->array && (*command)->array[y + 1])
+		{
+			(*command)->array[0] = ft_strjoin((*command)->array[y], " ");
+			(*command)->array[0] = ft_strjoin((*command)->array[0], (*command)->array[y + 1]);
+			y++;
+		}
+		lexer_parser_executer((*command)->array[0], 0, &_env);
+	}
 }
